04c183dfa71f3cfa350573f9f0f9f98c58755c20
s ᴇ x ʏ ꔪ 𝙲𝚘𝚍𝚎𝚛𝚜 <developersxmeetup@gmail.com>
2026-02-04 00:53:07 +0530

    Update tgcalls.py

--- Branded/modules/utilities/tgcalls.py
+++ Branded/modules/utilities/tgcalls.py
@@ -1,26 +1,20 @@
-from pytgcalls.types import Update, StreamAudioEnded
-
-from . import queues
 from ..clients.clients import call
+from . import queues
 from .streams import run_stream, close_stream
 
 
 async def run_async_calls():
 
-    @call.on_update()
-    async def stream_handler(_, update: Update):
+    @call.on_stream_end()
+    async def stream_end_handler(_, chat_id: int):
+        queues.task_done(chat_id)
 
-        # When current audio stream ends
-        if isinstance(update, StreamAudioEnded):
-            chat_id = update.chat_id
-            queues.task_done(chat_id)
+        if queues.is_empty(chat_id):
+            return await close_stream(chat_id)
 
-            if queues.is_empty(chat_id):
-                return await close_stream(chat_id)
+        data = queues.get(chat_id)
+        file = data["file"]
+        stream_type = data["type"]
 
-            data = queues.get(chat_id)
-            file = data["file"]
-            stream_type = data["type"]
-
-            stream = await run_stream(file, stream_type)
-            return await call.play(chat_id, stream)
+        stream = await run_stream(file, stream_type)
+        return await call.play(chat_id, stream)
